2b40b3e83eb59793cb5329685c77516f4a1fd152
blvckeasy <islomabdurakhmonov8@gmail.com>
2023-12-25 18:24:10 +0500

    updated

--- src/server.ts
+++ src/server.ts
@@ -17,55 +17,66 @@ import path from 'path';
 async function bootstrap() {
     // const PORT = ConfigService.get<number>("serverOptions.PORT");
     const PORT = process.env.PORT;
-    const HOST = ConfigService.get<string>("serverOptions.HOST");
-    const PROTOCOL = ConfigService.get<string>("serverOptions.PROTOCOL");
 
-    const app = express();
-    const httpServer = http.createServer(app);
+    const app = express()
 
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers: [graphqlScalarTypes, ...resolvers],
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    });
-
-    await botBootstrap();
-    await connectDatabase();
-    await server.start();
-
-    app.use(cors<cors.CorsRequest>({
-        origin: "*",
-    }))
-
-    app.use('/thumbnail_url', express.static(path.join(process.cwd(), 'uploads', 'images')))
-    app.use('/course_video', express.static(path.join(process.cwd(), 'uploads', 'videos')))
-
-    app.use('/graphql', express.json(), expressMiddleware(server, {
-        context: ({ req }) => ({ req }) as any,
-    }));
-
-    app.get("/", (req, res) => {
-        res.send("working");
+    app.get("/", (req, res, next) => {
+        res.send("ok");
     })
 
-    app.get("/api/helloworld", (req, res) => {
-        res.send("hello world")
+    app.listen(PORT, () => {
+        console.log("server is listening on *", PORT);
     })
 
-    await Routes(app);
+    // const HOST = ConfigService.get<string>("serverOptions.HOST");
+    // const PROTOCOL = ConfigService.get<string>("serverOptions.PROTOCOL");
 
-    app.use("**", (req: Request, res: Response, next: NextFunction) => {
-        res.send("404 not found");
-    })
+    // const app = express();
+    // const httpServer = http.createServer(app);
 
-    app.use(async (error: Error, req: Request, res: Response, next: NextFunction) => {
-        console.log("Rest api error handler")
-        console.error(error);
-        res.send("something went wrong")
-    })
+    // const server = new ApolloServer({
+    //     typeDefs,
+    //     resolvers: [graphqlScalarTypes, ...resolvers],
+    //     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    // });
 
-    httpServer.listen({ port: PORT });
-    console.log(`🚀 Server ready at  ${ PROTOCOL }://${ HOST }:${ PORT }`);
+    // await botBootstrap();
+    // await connectDatabase();
+    // await server.start();
+
+    // app.use(cors<cors.CorsRequest>({
+    //     origin: "*",
+    // }))
+
+    // app.use('/thumbnail_url', express.static(path.join(process.cwd(), 'uploads', 'images')))
+    // app.use('/course_video', express.static(path.join(process.cwd(), 'uploads', 'videos')))
+
+    // app.use('/graphql', express.json(), expressMiddleware(server, {
+    //     context: ({ req }) => ({ req }) as any,
+    // }));
+
+    // app.get("/", (req, res) => {
+    //     res.send("working");
+    // })
+
+    // app.get("/api/helloworld", (req, res) => {
+    //     res.send("hello world")
+    // })
+
+    // await Routes(app);
+
+    // app.use("**", (req: Request, res: Response, next: NextFunction) => {
+    //     res.send("404 not found");
+    // })
+
+    // app.use(async (error: Error, req: Request, res: Response, next: NextFunction) => {
+    //     console.log("Rest api error handler")
+    //     console.error(error);
+    //     res.send("something went wrong")
+    // })
+
+    // httpServer.listen({ port: PORT });
+    // console.log(`🚀 Server ready at  ${ PROTOCOL }://${ HOST }:${ PORT }`);
 }
 
 bootstrap();
